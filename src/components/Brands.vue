6c06b52179663c09b6cd3447fdacb6ee1e120100
Varduhi <varduhigalstyann71@gmail.com>
2024-11-28 16:17:23 +0400

    Api 12, 15, 18

--- src/components/Brands.vue
+++ src/components/Brands.vue
@@ -1,318 +1,73 @@
-<template >
+<template>
     <div class="top">
-        <span style="color: red ;">Below is the list of the brands we are an authorized distributor of.</span>
-        <span style="color: #6c6c6c;">Our users can request 3D models, drowings, or quotes of the product manufactured by the listed companies.</span>
+      <div v-html="brandInfo.content"></div>
+
     </div>
+  
     <div class="max">
-        <div class="cell">
-            <img src="../assets/Brands/1simatec-single-pos-c.jpg" alt="simatec-single">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/2Helukabel.jpg" alt="Helukabel">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/3airtac-logo.jpg" alt="Aritac">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/4Siemens-Logo.png" alt="Siemens">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/5Flender.png" alt="Flends">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/6Nidec-DCT-logo.jpg" alt="Nides">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/7weg_logo.jpg" alt="7weg_logo">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/8Bonfiglioli_logo.jpg" alt="8Bonfiglioli_logo">
-        </div>
-        <div class="cell">
-            <img style="height: 140px !important;" src="../assets/Brands/9Wittenstein_Logo.png" alt="9Wittenstein_Logo">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/10DESMI-Pumps-and-Pumping-Systems.jpg" alt="10DESMI">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/11brand1.jpg" alt="11brand1">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/12elektrim.jpg" alt="12elektrim">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/13Stober.png" alt="13Stober">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/14HKK_Chain.jpg" alt="14HKK_Chain">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/15formsprag-clutch-logo.jpg" alt="15formsprag">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/16Rossi.jpg" alt="16Rossi">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/17Ringspann.png" alt="17Ringspann">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/18Thomson-logo.png" alt="18Thomson">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/19Toshiba-Logo.png" alt="19Toshiba">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/20Warner-Electric.jpg" alt="20Warner">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/21Bando-logo.png" alt="21Bando">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/22MGM-Electric-Motors.png" alt="22MGM">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/23Heidrive.jpg" alt="23Heidrive">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/24Martin-Engineering-logo.jpg" alt="24Martin">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/25logo_ringfeder.png" alt="25logo_ringfeder">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/26Brook-Crompton.jpg" alt="26Brook">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/27sew-eurodrive.png" alt="27sew">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/28bauer-gear-logo.png" alt="28bauer">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/29winkel.png" alt="29winkel">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/30Glogo.jpg" alt="30Glogo">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/31schneeberger-logo.png" alt="31schneeberger">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/32BrotherGearmotors.jpg" alt="32BrotherGearmotors">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/33Apex_USA.jpg" alt="33Apex_USA">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/34stromag.jpg" alt="34stromag">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/35festo_logo.png" alt="35festo_logo">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/36logo-fachini-speed-reducers-gears-couplings-italy-2.png" alt="36logo">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/37Perma_logo.png" alt="37Perma_logo">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/38Himmel.png" alt="38Himmel">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/39logo-header.png" alt="39logo">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/40GAM.png" alt="40GAM">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/41eich-rollenlager-logo.jpg" alt="41eich">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/42LafertNorthAmerica.jpg" alt="42LafertNorthAmerica">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/43TSLOTS-Logo.png" alt="43TSLOTS">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/44Motovario_logo.png" alt="44Motovario_logo">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/45Girard-Transmissions-logo.jpg" alt="45Girard">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/46Foot-master-logo.png" alt="46Foot">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/47NTN01_corp.jpg" alt="47NTN01_corp">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/48kobold-logo.jpg" alt="48kobold">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/49nippon-bearing-logo.gif" alt="49nippon">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/50Cylinder.jpg" alt="50Cylinder">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/51Ameridrive.png" alt="51Ameridrive">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/52gdfgsdf.png" alt="52gdfgsdf">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/53aurora-logo.jpg" alt="53aurora">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/54malmedie-logo.jpg" alt="54malmedie">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/55Vibco-vibrator-logo.jpg" alt="55Vibco">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/56Sacemi-Gamar.png" alt="56Sacemi">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/57marland-clutch.png" alt="57marland">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/58IKO_Logo_New.png" alt="58IKO_Logo_New">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/59Maurey-Logo.jpg" alt="59Maurey">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/60nuttall-gear-logo.png" alt="60nuttall">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/61bestorq.png" alt="61bestorq">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/62Bega-logo.png" alt="62Bega">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/63Stieber-clutch.png" alt="63Stieber">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/64E.png" alt="64E">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/65Dalton.png" alt="65Dalton">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/66memolub.png" alt="66memolub">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/67logo-footer-3.png" alt="67logo">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/68marzocchipompe.png" alt="68marzocchipompe">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/69svendbordg-brakes-(1).png" alt="69svendbordg">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/70PTI-Logo.png" alt="70PTI">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/71Cinchseal-distributor.png" alt="71Cinchseal">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/72Allied-Locke-Industries.png" alt="72Allied">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/73fyh-logo.png" alt="73fyh">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/74brunel_logo.png" alt="74brunel_logo">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/75gates-logo.jpg" alt="75gates">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/76NAE-Logo-Hi-Res_0.png" alt="76NAE">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/77Rosta.png" alt="77Rosta">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/78SITI-USA-logo.png" alt="78SITI">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/79MASTERDRIVELOGO.jpg" alt="79MASTERDRIVELOGO">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/80Logo-2016-4C.jpg" alt="80Logo">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/81Techtop-logo.png" alt="81Techtop">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/82stafford-mfg-logo.png" alt="82stafford">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/83Scheerer-Bearing.jpg" alt="83Scheerer">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/84RBI-Bearing-Logo.png" alt="84RBI">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/85TB-Woods.png" alt="85TB">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/86Accushim-logo.png" alt="86Accushim">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/87Nachi-Logo.svg.png" alt="87Nachi">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/88COC_Turtle_blue.png" alt="88COC_Turtle_blue">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/89smith-bearing-logo.jpg" alt="89smith">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/90Isostatic-logo.jpg" alt="90Isostatic">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/91TMX_logo.jpg" alt="91TMX_logo">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/92Douglas-logo.jpg" alt="92Douglas">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/93Twiflex.jpg" alt="93Twiflex">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/94fag.png" alt="94fag">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/95Continental.png" alt="95Continental">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/96Guardian-Coupling.png" alt="96Guardian">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/97Atek_logo.png" alt="97Atek_logo">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/98Continentalcontitechlogo400x105.jpg" alt="98Continentalcontitechlogo400x105">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/99inertia-dynamics-logo.png" alt="99inertia">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/100Orthman-logo.png" alt="100Orthman">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/101NSK-Logo.jpg" alt="101NSK">
-        </div>
-        <div class="cell">
-            <img src="../assets/Brands/102Mitsuboshi-belt.png" alt="102Mitsuboshi">
-        </div>
-        
+      <div v-for="brand in brands" :key="brand.id" class="cell">
+        <img :src="`https://hvhindustrial.com/images/frontend_images/brands/${brand.filename}`" :alt="brand.alt" />
+      </div>
     </div>
 </template>
+
+<script>
+import axios from 'axios'; 
+
+export default {
+    data() {
+        return {
+            brands: [], 
+            brandInfo: { content: '' },
+        };
+    },
+  
+  computed: {
+    token() {
+      return this.$store.state.token; 
+    },
+  },
+   
+  mounted() {
+    this.fetchBrandsInfo();
+    this.fetchBrands();
+  },
+
+  methods: {
+    async fetchBrandsInfo() {
+      try {
+        const response = await axios.post('https://webapi.hvhdesigner.com/api/get-brand-info', {}, {
+          headers: {
+            'Authorization': `Bearer ${this.token}`,
+          },
+        });
+
+        if (response.data.status === "true") {
+          this.brandInfo = response.data.info; // Storing brand info (content)
+        }
+      } catch (error) {
+        console.error('Error fetching brand information:', error);
+      }
+    },
+    
+    async fetchBrands() {
+      try {
+        const response = await axios.post('https://webapi.hvhdesigner.com/api/get-brands', {}, {
+          headers: {
+            'Authorization': `Bearer ${this.token}`, 
+          },
+        });
+
+        if (response.data.status === "true") {
+          this.brands = response.data.brands; 
+        }
+      } catch (error) {
+        console.error('Error fetching brands:', error);
+      }
+    },
+  },
+}
+</script>
 
 <style scoped>
 .top{
