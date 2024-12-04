231d2d64c90855e26590841cdcc63f569cc12024
Varduhi <varduhigalstyann71@gmail.com>
2024-12-04 18:59:09 +0400

    verj vae

--- src/App.vue
+++ src/App.vue
@@ -31,11 +31,13 @@ export default {
     const siteData = computed(() => store.getters.getSettings || {});
 
     useHead({
-      title: computed(() => siteData.value.meta_title || 'huhbhb'),
+      title: computed(() => siteData.value.meta_title || 'huhbhjhb'),
       meta: [
         {
           name: `description`,
-          content: computed(() => siteData.value.meta_description || ''),
+          content: computed(() => siteData.value.meta_description || 'vgvjhg'),
+          name: `keywords`,
+          content: computed(() => siteData.value.meta_keywords || 'rsyerdhg'),
         },
       ],
     });
